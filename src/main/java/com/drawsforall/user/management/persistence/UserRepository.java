18f986389c653fe1c43596c316c9238b06eaf8fe
Eduardo Berrospe <berrospe.he@gmail.com>
2019-11-12 00:07:59 -0600

    added ignore case when finding a user by email

--- src/main/java/com/drawsforall/user/management/persistence/UserRepository.java
+++ src/main/java/com/drawsforall/user/management/persistence/UserRepository.java
@@ -2,15 +2,12 @@ package com.drawsforall.user.management.persistence;
 
 import com.drawsforall.user.management.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmailLike(String email);
+    Optional<User> findByEmailIgnoreCase(String email);
 }
