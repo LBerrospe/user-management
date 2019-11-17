1fbe3d8de6d505ebc08b630619a9709bec8164e0
Eduardo Berrospe <berrospe.he@gmail.com>
2019-11-16 23:46:39 -0600

    added responseEntity in UserController

--- src/main/java/com/drawsforall/user/management/web/rest/UserController.java
+++ src/main/java/com/drawsforall/user/management/web/rest/UserController.java
@@ -1,11 +1,11 @@
 package com.drawsforall.user.management.web.rest;
 
 import com.drawsforall.user.management.business.UserService;
-import com.drawsforall.user.management.security.ApiResponse;
 import com.drawsforall.user.management.web.rest.dto.PagedUsersDTO;
 import com.drawsforall.user.management.web.rest.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.Map;
 
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
@@ -36,34 +36,37 @@ public class UserController {
 
     @Secured({"ROLE_ADMIN", "ROLE_USER_CREATE", "ROLE_ANONYMOUS"})
     @PostMapping(produces = APPLICATION_JSON_VALUE)
-    public ApiResponse createUser(@Valid @RequestBody UserDTO userDTO) {
-        return new ApiResponse(CREATED.value(), "User " + userDTO.getEmail() + " created", userService.createUser(userDTO));
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
+        UserDTO createdUser = userService.createUser(userDTO);
+        return ResponseEntity
+                .created(UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(createdUser.getId()).toUri())
+                .body(createdUser);
     }
 
     @Secured("ROLE_ADMIN")
     @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public PagedUsersDTO getUsers(
+    public ResponseEntity<PagedUsersDTO> getUsers(
             @RequestParam(name = "page", defaultValue = "0") Integer page,
             @RequestParam(name = "size", defaultValue = "20") Integer size
     ) {
-        return userService.getUsers(page, size);
+        return ResponseEntity.ok(userService.getUsers(page, size));
     }
 
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping(value = "/lookup", produces = APPLICATION_JSON_VALUE)
-    public UserDTO lookupUser(
+    public ResponseEntity<UserDTO> lookupUser(
             @RequestParam(name = "by", defaultValue = "id") String by,
             @RequestParam(name = "value") String value
     ) {
-        return userService.lookupUser(by, value);
+        return ResponseEntity.ok(userService.lookupUser(by, value));
     }
 
     @Secured({"ROLE_ADMIN", "ROLE_USER_UPDATE"})
     @PatchMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
-    public UserDTO updateUser(
+    public ResponseEntity<UserDTO> updateUser(
             @PathVariable Long id,
             @RequestBody Map<String, Object> fieldsToUpdate
     ) {
-        return userService.updateUser(id, fieldsToUpdate);
+        return ResponseEntity.ok(userService.updateUser(id, fieldsToUpdate));
     }
 }
