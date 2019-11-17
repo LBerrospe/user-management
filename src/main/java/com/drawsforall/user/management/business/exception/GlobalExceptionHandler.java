4fef51476649b16438aa0c7969e8fabbbbbc7407
Eduardo Berrospe <berrospe.he@gmail.com>
2019-11-17 00:56:57 -0600

    added controller advice for error handling

---
+++ src/main/java/com/drawsforall/user/management/business/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.drawsforall.user.management.business.exception;
+
+import com.drawsforall.user.management.web.rest.dto.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> customHandleNotFound(Exception ex, WebRequest request) {
+
+        ErrorResponseDTO errors = new ErrorResponseDTO();
+        errors.setTimestamp(LocalDateTime.now());
+        errors.setError(ex.getMessage());
+        errors.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/drawsforall/user/management/web/rest/dto/ErrorResponseDTO.java
@@ -0,0 +1,19 @@
+package com.drawsforall.user.management.web.rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class ErrorResponseDTO {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+}
