6514194ecabedc40d8510a1f9aec59ea49e2e63e
Eduardo Berrospe <berrospe.he@gmail.com>
2019-11-17 15:14:48 -0600

    returning validations for userEntity

--- src/main/java/com/drawsforall/user/management/persistence/entity/User.java
+++ src/main/java/com/drawsforall/user/management/persistence/entity/User.java
@@ -12,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Setter
@@ -24,12 +27,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email(message = "Please provide a valid email.")
+    @NotBlank(message = "Please provide an email")
+    @Size(max = 128, message = "Email must contain less than 128 characters")
     private String email;
 
+    @NotBlank(message = "Please provide a password")
+    @Size(min = 5, max = 255, message = "Password must have at least 5 characters")
     private String password;
 
+    @NotBlank(message = "Please provide a first name")
+    @Size(max = 32, message = "First name must contain less than 32 characters")
     private String firstName;
 
+    @NotBlank(message = "Please provide a last name")
+    @Size(max = 32, message = "Last name must contain less than 32 characters")
     private String lastName;
 
     @ManyToMany(fetch = FetchType.EAGER)
--- src/main/java/com/drawsforall/user/management/web/rest/dto/UserDTO.java
+++ src/main/java/com/drawsforall/user/management/web/rest/dto/UserDTO.java
@@ -17,21 +17,21 @@ public class UserDTO extends ResourceSupport {
 
     private Long userId;
 
-    @Size(max = 128, message = "Email must contain less than 128 characters")
     @Email(message = "Please provide a valid email.")
     @NotBlank(message = "Please provide an email")
+    @Size(max = 128, message = "Email must contain less than 128 characters")
     private String email;
 
-    @Size(min = 5, max = 255, message = "Password must have at least 5 characters")
     @NotBlank(message = "Please provide a password")
+    @Size(min = 5, max = 255, message = "Password must have at least 5 characters")
     private String password;
 
-    @Size(max = 32, message = "First name must contain less than 32 characters")
     @NotBlank(message = "Please provide a first name")
+    @Size(max = 32, message = "First name must contain less than 32 characters")
     private String firstName;
 
-    @Size(max = 32, message = "Last name must contain less than 32 characters")
     @NotBlank(message = "Please provide a last name")
+    @Size(max = 32, message = "Last name must contain less than 32 characters")
     private String lastName;
 
     private String display;
